5b738bf6993f22ac8f59eb7d2dce72cbe6408d20
hk <kiptooharon.hk@gmail.com>
2023-06-15 22:49:41 +0300

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-/*puts function==print*/
+/**
+* main - puts function library func inside main
+* Return: 0
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
