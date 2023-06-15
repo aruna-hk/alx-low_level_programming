1fae9f39cedc1a99cd61bfd375fcfe1bd99e7d4d
hk <kiptooharon.hk@gmail.com>
2023-06-16 00:20:01 +0300

    --sizes of c datatypes-hk

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float e;
-printf("size of a char: %d byte(s)\n", (unsigned)sizeof(a));
-printf("size of an int: %d byte(s)\n", (unsigned)sizeof(b));
-printf("size of a long int: %d byte(s)\n", (unsigned)sizeof(c));
-printf("size of a long long int: %d byte(s)\n", (unsigned)sizeof(d));
-printf("size of a float: %d byte(s)\n", (unsigned)sizeof(e));
+printf("size of a char: %d byte(s)\n", (unsigned int)sizeof(a));
+printf("size of an int: %d byte(s)\n", (unsigned int)sizeof(b));
+printf("size of a long int: %d byte(s)\n", (unsigned int)sizeof(c));
+printf("size of a long long int: %d byte(s)\n", (unsigned int)sizeof(d));
+printf("size of a float: %d byte(s)\n", (unsigned int)sizeof(e));
 return (0);
 }
