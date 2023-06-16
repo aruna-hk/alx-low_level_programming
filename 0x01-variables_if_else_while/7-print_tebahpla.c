55c08a24b87998e6eedae448b23cd06def7dba57
hk <kiptooharon.hk@gmail.com>
2023-06-17 00:47:44 +0300

    reversing alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - lowercase f
+ * Description: prints lower in reverse order
+ * Return: 0
+*/
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
