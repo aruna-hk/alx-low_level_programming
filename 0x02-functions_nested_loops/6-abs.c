60a46972b61bd4ffafd9e7728a33479ab1fc2c37
hk <kiptooharon.hk@gmail.com>
2023-06-20 22:37:59 +0300

    absolute value of a nummber

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - lowecase
+* @c: ascii num
+* Return: 1 if tue 0 if false
+*/
+int _abs(int r)
+{
+	if (r < 0)
+		r = r*(-1);
+		_putchar('r');
+	else
+		_putchar('r');
+	return (0);
+}
