dff83c3ac0138d9c268db375392a5da59928ea67
hk <kiptooharon.hk@gmail.com>
2023-06-21 01:29:53 +0300

    counting to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,6 +7,8 @@
 */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n >= 98)
 	{
 		if (n == 98)
@@ -16,14 +18,27 @@ void print_to_98(int n)
 			for (i = n; i >= 98; i--)
 			{
 				printf("%d", i);
+				if (i == 98)
+				{
+					_putchar('\n');
+					continue;
+				}
 				_putchar(44);
 				_putchar(' ');
 			}
 		}
 	else
+	{
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
+			if (i == 98)
+			{
+				_putchar('\n');
+				continue;
+			}
 			_putchar(44);
 			_putchar(' ');
+		}
+	}
 }
