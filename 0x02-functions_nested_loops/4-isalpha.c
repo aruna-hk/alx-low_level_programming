94ac61060c9a7cd979b3452ba7daafedb792e2d7
hk <kiptooharon.hk@gmail.com>
2023-06-20 22:02:22 +0300

    checking if a letter-

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha - lowecase
+* @c: ascii num
+* Return: 1 if tue 0 if false
+*/
+int _isalpha(int c)
+{
+	if (c > 97 && c <= 122 || c > 64 && c <==90)
+		return (1);
+	else
+		return (0);
+}
