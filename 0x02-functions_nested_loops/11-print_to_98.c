d110af29507861b879496bb4cc2ff92ceae00459
hk <kiptooharon.hk@gmail.com>
2023-06-21 01:05:57 +0300

    --iterating to 98-+

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 
 		while (n >= 98)
 		{
-			_putchar(n);
+			printf("%d", n);
 			if (n == 98)
 			{
 				_putchar('\n');
@@ -29,7 +29,7 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			_putchar(n);
+			printf("%d", n);
 			if (n == 98)
 			{
 				_putchar('\n');
@@ -44,5 +44,5 @@ void print_to_98(int n)
 		}
 	}
 	else
-		_putchar('\n');
+		printf("%d\n");
 }
