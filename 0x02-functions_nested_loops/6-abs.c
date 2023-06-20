3109013573675cb552c03bd2abef6c5ae69a6ab2
hk <kiptooharon.hk@gmail.com>
2023-06-20 22:46:28 +0300

    -absolute value of a nummber

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
 * _abs - lowecase
-* @c: ascii num
-* Return: 1 if tue 0 if false
+* @r: ascii num
+* Return: 0
 */
 int _abs(int r)
 {
 	if (r < 0)
 	{
-		r = r*(-1);
+		r = r * -1;
 		_putchar(r);
 	}
 	else
