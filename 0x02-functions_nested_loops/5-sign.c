934ec53ef5384aeff803d6a4fb48cc00ac6c3a7b
hk <kiptooharon.hk@gmail.com>
2023-06-20 22:23:08 +0300

    -checking sign of number--

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,18 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		_putchar(',');
+	}
 		return (1);
 	else if (n == 0)
+	{
 		_putchar('0');
 		_putchar(',');
-
 		return (0);
-	else if (n < 0)
+	}
+	else
+	{
 		_putchar('-');
 		_putchar(',');
 		return (-1);
+	}
 }
