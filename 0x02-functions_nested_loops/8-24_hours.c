84fb0ad584ca711ea279ffb740ea74f8244010db
hk <kiptooharon.hk@gmail.com>
2023-06-20 23:25:19 +0300

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,13 @@ void jack_bauer(void)
 	int i;
 	int j;
 
-	for (i=48;i<==53;i++)
+	for (i=48;i<=53;i++)
 	{
 		for (j = 48;j <= 57; j++)
 		{
 			_putchar(i);
 			_putchar(j);
-			_putchar('\n')
+			_putchar('\n');`
 		}
 	}
 }
