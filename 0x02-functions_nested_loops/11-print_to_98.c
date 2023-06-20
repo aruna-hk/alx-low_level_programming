55e41f16f010db1ea16811a41987e924048669ea
hk <kiptooharon.hk@gmail.com>
2023-06-21 01:01:45 +0300

    --iterating to 98-+

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,12 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				_putchar('\n');
-				continue;
+				break;
 			}
-			_putchar(44);
-			_putchar(' ');
-			n = n - 1;
+			else
+				_putchar(44);
+				_putchar(' ');
+				n = n - 1;
 		}
 	}
 	else if (n < 98)
@@ -30,10 +31,12 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				_putchar('\n');
-				continue;
+				break;
 			}
-			_putchar(44);
-			_putchar(' ');
+			else
+				_putchar(44);
+				_putchar(' ');
+				n = n + 1;
 		}
 	}
 	else
