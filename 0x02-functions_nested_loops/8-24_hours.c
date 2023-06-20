9f7c3785872032fff6c9b2667e1064009e5002d4
hk <kiptooharon.hk@gmail.com>
2023-06-20 23:23:54 +0300

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+/**
+* jack_bauer-time counter 
+*/
+void jack_bauer(void)
+{ 
+	int i;
+
+	for (i=48;i<==53;i++)
+	{
+		for (j = 48;j <= 57; j++)
+		{
+			_putchar(i);
+			_putchar(j);
+			_putchar('\n')
+		}
+	}
+}
