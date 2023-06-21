f3981152a3403d27385525b97edf6e1504395ec0
hk <kiptooharon.hk@gmail.com>
2023-06-21 03:28:22 +0300

    9 by 9 multiplication tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+* times_table -multiplication table
+*/
 void times_table(void)
 {
-	 int a, b;
-    
+	int a,
+	int b;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			printf("%d, ..%d", a*b,b);
+			printf("%d, %d", a * b, b);
 			if (b != 9)
 				printf(",");
 		}
 		printf("\n");
 	}
-    
 }
