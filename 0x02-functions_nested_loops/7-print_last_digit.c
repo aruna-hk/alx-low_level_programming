593e9601844454112089dead484505d4845aeab9
hk <kiptooharon.hk@gmail.com>
2023-06-21 17:41:38 +0300

    print last number-

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,17 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		_putchar(n);
+		_putchar('n');
 		return ((n % 10) * -1);
 	}
 	else if (n > 0)
+	{
+		_putchar('n');
 		return (n % 10);
+	}
 	else
+	{
+		_putchar("48");
 		return (0);
+	}
 }
