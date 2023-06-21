4204e83f284905253809dd52e5bdcbe7ff89fe6a
hk <kiptooharon.hk@gmail.com>
2023-06-21 03:03:53 +0300

    9 by 9 multiplication tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			printf("%d", a * b);
-			if( a == 0 || b == 0||b < 5  && a < 4 )
+			if( a == 0 || b == 0||(b < 5  && a < 4) )
 			{
 			 	printf(",  ");
 			
