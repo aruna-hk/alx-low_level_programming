6cd6dd912a1f90386e45da05e3167fcbf66333a9
hk <kiptooharon.hk@gmail.com>
2023-06-21 20:38:32 +0300

    sum of multiples of 3en 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,10 +14,10 @@ int main(void)
 		if (i % 3  == 0 || i % 5 == 0)
 		{
 			sum = sum + i;
-			printf("%d", sum);
 		}
 		else
 			continue;
 	}
-	return (sum);
+	printf("%d\n", sum)
+	return (0);
 }
