6335d14f73573f93f39b5bb047bd8cf16328727b
hk <kiptooharon.hk@gmail.com>
2023-06-21 03:39:34 +0300

    clock count

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,34 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* jack_bauer-time counter 
+* jack_bauer-time counter
 */
 void jack_bauer(void)
-{ 
+{
 	int i;
 	int j;
-	int l = 48;
-	int m = 48;
 
-	for (i = 48; i <= 50; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 48;j <= 51; j++)
+		for (j = 0; j <= 49; j++)
 		{
-			while (l <= 53)
-			{
-				while (m <=57)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(l);
-					_putchar(m);
-					_putchar('\n');
-					m = m + 1;
-					if (m == 9)
-						continue;
-				}
-				l = l + 1;
-			}
+			printf("%d:%d", i j);
 		}
 	}
 }
