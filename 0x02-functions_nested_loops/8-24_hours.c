f5bd439ff7e477563ad22513c7adb89b21f2acba
hk <kiptooharon.hk@gmail.com>
2023-06-21 03:51:34 +0300

    clock count

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,14 @@ void jack_bauer(void)
 	{
 		for (j = 0; j <= 49; j++)
 		{
-			printf("%d:%d", i, j);
+			if (i < 10 && j < 10)
+				printf("0%d:0%d\n", i, j);
+			else if (i < 10)
+				printf("0%d:%d\n", i, j);
+			else if (j < 10)
+				printf("%d:0%d\n", i, j);
+			else
+				printf("%d:%d", i, j);
 		}
 	}
 }
