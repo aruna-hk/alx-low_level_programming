ecfa302036aac673cf239448d9e3ff16536b6169
hk <kiptooharon.hk@gmail.com>
2023-06-21 21:44:38 +0300

    first 98 of fib sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - fibonacci sequence
+* Return: 0
+*/
+int main(void)
+{
+	signed long int i = 1;
+	signed long int i_2 = 2;
+	signed long int a;
+	signed long int next;
+	int counter = 0;
+
+	printf("%ld, %ld, ", i, i_2);
+	for (a = 3; a <= 100; a++)
+	{
+		next = i + i_2;
+		printf("%ld", next);
+		counter = counter + 1;
+		if ( a== 98)
+			break;
+		printf(", ");
+		i = i_2;
+		i_2 = next;
+	
+	}
+	printf("\n");
+	return (0);
+}
