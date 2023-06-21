eb5fa9e26f62920b5d3dc2605fe51dd460bb4d99
hk <kiptooharon.hk@gmail.com>
2023-06-21 19:58:23 +0300

    sum of multiples of 3en 5

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_time_tables - multiplication tables
+* @n: - dimensions
+* Return: 0
+*/
+int print_time_table(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= n; i++)
+	{
+		fo (j = 0; j <= n; j++)
+		{
+			
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+/**
+* main - entry
+* @i: int arg
+* Return: sum of multiples of 3 and 5
+*/
+int main(int i)
+{
+	int sum;
+	int i;
+
+	sum = 0;
+	for (i = 0; i <= 1024; i++)
+	{
+		m = i % 3;
+		k = i % 5
+		if (m == 0 || k == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	return (sum);
+}
