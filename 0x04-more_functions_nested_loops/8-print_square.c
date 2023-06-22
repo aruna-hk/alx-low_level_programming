f55eec0fac088aebf923420d9ba8180bbb05b1d0
hk <kiptooharon.hk@gmail.com>
2023-06-22 20:41:41 +0300

    print square of char--

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,10 +8,13 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-			_putchar(35);
-		_putchar('\n');
-	}
+	if (size == 0)
+		_putchar("\n");
+	else
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar(35);
+			_putchar('\n');
+		}
 }
