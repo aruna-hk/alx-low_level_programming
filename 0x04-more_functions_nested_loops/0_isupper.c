6d76b7b9d4f7bd7b467da310dbd259e1ebe9a11c
hk <kiptooharon.hk@gmail.com>
2023-06-22 22:59:00 +0300

    function to check for case-

--- 0x04-more_functions_nested_loops/0_isupper.c
+++ 0x04-more_functions_nested_loops/0_isupper.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
 * _isupper - check if ASCII value passed in is uppercase
 * @c: ASCII value
 * Return: 1 if c is caps else 0
 */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 void print_diagonal(int n);
+int _isupper(int c);
 int _isdigit(int c);
 void print_triangle(int size);
 void print_square(int size);
@@ -7,7 +8,6 @@ void more_numbers(void);
 void print_most_numbers(void);
 void print_numbers(void);
 int mul(int x, int y);
-int _isupper(int c);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
