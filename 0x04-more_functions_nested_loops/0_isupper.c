1cefa476b9218f58a284c36efdf8ad3b1702f7fb
hk <kiptooharon.hk@gmail.com>
2023-06-22 15:04:03 +0300

    check IF UPPER function update

--- 0x04-more_functions_nested_loops/0_isupper.c
+++ 0x04-more_functions_nested_loops/0_isupper.c
@@ -5,9 +5,9 @@
 * @c ascii: value
 * Return: 1 if c is caps else 0
 */
-int _isupper(int v)
+int _isupper(int c)
 {
-	if (v >= 65 && v <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
