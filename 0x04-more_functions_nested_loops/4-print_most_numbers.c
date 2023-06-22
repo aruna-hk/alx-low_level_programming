df745c8106f8f42483bd75d94576bf6861317875
hk <kiptooharon.hk@gmail.com>
2023-06-22 15:42:25 +0300

    added funct-list digit execp 2&4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,8 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i == 51 || i == 53)
-			continue;
-		_putchar(i);
+		if (i != 51 || i != 53)
+			_putchar(i);
 	}
 	_putchar('\n');
 }
