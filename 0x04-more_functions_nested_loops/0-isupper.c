b1a6ff98218046f35e5894aa42d75d4c1e752648
hk <kiptooharon.hk@gmail.com>
2023-06-22 23:12:01 +0300

    function to check if enterered char is lower or upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isupper - check if uppercase or lower
+* @c: input
+* Return: 1 if c is caps else 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
