d98b0683f0b706d92be7af93a35a6382cf4121de
hk <kiptooharon.hk@gmail.com>
2023-06-23 04:59:56 +0300

    leap year functions -- debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 
+int main(void);
+
 int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
 
 void print_remaining_days(int month, int day, int year);
 
