c04974d78d1299b8518e0b8dc65387474e9f4361
hk <kiptooharon.hk@gmail.com>
2023-06-26 21:42:39 +0300

    fuction that takes string literal as input and display them in std out

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* _puts - display the str passed
-* @str: -take the string
+* print_rev - print arg in reverse
+* @s: pointer to the string literal to operate on
 */
 void print_rev(char *s)
 {
-	for (*s = '\0'-1;;s--)
-		_putchar(s);
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len = len + 1;
+		*s++;
+	}
+	while (len >= 0)
+	{
+		_putchar(*s);
+		*s--;
+		len--;
+	}
 	_putchar('\n');
 }
-int main(void)
-{
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-}  
