cc64ec16a5fe416e5594dad688dbaeec3134d421
hk <kiptooharon.hk@gmail.com>
2023-06-26 16:43:59 +0300

    function that takes adress of var and change the value init

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* reset_to_98 - takes pointer and reset value in that memory location
+* @*n: pointer to mem location of the param
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+topic--pointers arrays and strings
+date 26/june/2023
+author-kiptooharon.hk@gmail.com
+INTO POINTERS ARRAYS AND STRINGS repo--dir
