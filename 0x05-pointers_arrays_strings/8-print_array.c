d2c7db0643c91c11445970d1e838d13ce6908b80
hk <kiptooharon.hk@gmail.com>
2023-06-27 01:53:45 +0300

    displaying elements of array using pointer arithmetic

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_array -- display elements of array using pointer arithmetic
+* @n: array--list
+* @a: pointer to array
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar (*(a + i));
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(32);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
