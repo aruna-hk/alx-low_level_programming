62e1857537b82a7a00c49e36ed11a5177aefe92b
hk <kiptooharon.hk@gmail.com>
2023-06-26 22:47:52 +0300

    function to parmanently reversing string other functions ---main cse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 /**
 * rev_string - reverse string
-* @s: pointer to the string literal to operate on
+* @s: pointer to the string literal to operate on--
 */
 void rev_string(char *s)
 {
 	int len = 0;
+	char *start = s;
+	char swapspace;
 
 	while (*s != '\0')
 	{
 		len = len + 1;
 		s++;
 	}
-	while (len > 0)
+	s--;
+
+	while (start < s)
 	{
-		putchar(*s);
+		swapspace = *start;
+		*start = *s;
+		*s = swapspace;
+		start++;
 		s--;
-		len--;
 	}
 	putchar('\n');
-}
-int main(void)
-{
-    char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
 }
 
