17976785a867f057f80bfaccd03d3ef25f299bfb
hk <kiptooharon.hk@gmail.com>
2023-06-27 04:09:50 +0300

    function --copies string to string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -32,13 +32,3 @@ char *_strcpy(char *dest, char *src)
 		src--;
 	}
 }
-int main(void)
-{
-    char s1[98];
-    char *ptr;
-
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-    return (0);
-}
