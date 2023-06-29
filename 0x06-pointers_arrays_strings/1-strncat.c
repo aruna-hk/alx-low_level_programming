fe15d03a2dc1f3b0eff65675c15acf41afbd2128
kiptoo haron <kiptooharon.hk@gmail.com>
2023-06-29 20:50:10 +0300

    string concatanation function--

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * _strcat - append string to the end of a string
  * @dest: - base str
@@ -8,7 +8,6 @@
 char *_strcat(char *dest, char *src)
 {
 	char *point_er;
-
 	point_er = dest;
 	while (*dest != 0)
 	{
@@ -23,4 +22,18 @@ char *_strcat(char *dest, char *src)
 	*dest = '\0';
 	point_er = dest;
 	return (point_er);
+}
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
