66fe21ff0a70746fe3712babdcfadce9795e9333
kiptoo haron <kiptooharon.hk@gmail.com>
2023-06-29 20:46:52 +0300

    string concatanation function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * _strcat - append string to the end of a string
- * @dest: - string to be added
+ * @dest: - base str
  * @src: - str to append to dest str
- * Return: *src
+ * Return: dest -- pointer to dest
 */
 char *_strcat(char *dest, char *src)
 {
 	char *point_er;
-
 	point_er = dest;
 	while (*dest != 0)
 	{
@@ -21,5 +20,20 @@ char *_strcat(char *dest, char *src)
 		src++;
 	}
 	*dest = '\0';
+	point_er = dest;
 	return (point_er);
+}
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,5 @@
 POINTERS,arrays,strings
 Author <kiptooharon.hk@gmail.com>
+1-strcat
+	This function appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
+
