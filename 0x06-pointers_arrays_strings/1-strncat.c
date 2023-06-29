bf131fe9e77e3df42b7102130d2a939b8c406c4d
kiptoo haron <kiptooharon.hk@gmail.com>
2023-06-30 00:07:24 +0300

    function to append specified bits to a string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _strcat - append string to the end of a string
+ * _strncat - append string to the end of a string
  * @dest: - base str
  * @src: - str to append to dest str
+ * @n: number of bytes to append
  * Return: dest -- pointer to dest
 */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
+	char *ptr = dest;
 
-	while (*dest !='\0')
+	while (*dest != '\0')
 	{
 		dest++;
 	}
-	while (i < n && *src != '\0')
+	while (i < n && (*src != '\0'))
 	{
 		*dest = *src;
 		dest++;
@@ -21,23 +23,5 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	*dest = '\0';
-	return (dest);
-}
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
+	return (ptr);
 }
