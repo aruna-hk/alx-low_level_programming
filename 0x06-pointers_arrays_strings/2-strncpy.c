40f6bca9976a6170065d5535073b7d344831faed
kiptoo haron <kiptooharon.hk@gmail.com>
2023-06-30 05:12:36 +0300

    function to append to start of a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _strncat - append string to the end of a string
- * @dest: - base str
- * @src: - str to append to dest str
+ * _strncpy - append string to the start of a string
+ * @dest: - destination string
+ * @src: - source string
  * @n: number of bytes to append
  * Return: dest -- pointer to dest
 */
-char* _strncpy(char* dest, char* src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 /******************************************************************
 *********************************************************************
 ************/
 	int i = 0;
 	char *ptr = dest;
+
 	while (i < n && *src != '\0')
 	{
 		*dest = *src;
@@ -20,40 +21,5 @@ char* _strncpy(char* dest, char* src, int n)
 		src++;
 		i++;
 	}
-	
-       return ptr;
-}
-
-int main(void)
-{
-    char s1[98];
-    char *ptr;
-    int i;
-
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
-    return (0);
+	return (ptr);
 }
