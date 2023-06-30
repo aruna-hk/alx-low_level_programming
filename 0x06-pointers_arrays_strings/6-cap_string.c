d6bcfd07ab0e3006d0522d093511f67b9a0d93d8
kiptoo haron <kiptooharon.hk@gmail.com>
2023-06-30 04:30:06 +0300

    function to capitalise after some specified delimiters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
- *
- *
- *
- *
+* cap_string -capitalize characters based on delimiters
+* @j: takes a strin as arg
+* Return: ptr
 */
+
 char *cap_string(char *j)
 {
-	char delimiter[]=",;.!?\"(){} \t\n";
-	char caps[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char small[]="abcdefghijklmnopqrstuvwxyz";
-	int i;
-	int m;
+	char *ptr = j;
+	char delimiter[] = ",;.!?\"(){} \t\n";
+	char caps[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char small[] = "abcdefghijklmnopqrstuvwxyz";
+	int i, m;
 
-	for(i = 0;i <= 26;i++)
+	for (i = 0; i < 26; i++)
 	{
-		if (*j = small[i])
+		if (*j == small[i])
 		{
 			*j = caps[i];
 			break;
@@ -23,37 +23,28 @@ char *cap_string(char *j)
 	}
 	while (*j != '\0')
 	{
-		for (m = 0; m <= 14;m++)
+		for (m = 0; m < 14; m++)
 		{
 			if (*j == delimiter[m])
 			{
 				j++;
-				for(i = 0;i <= 26;i++)
+				while (*j == ' ' || *j == '\t' || *j == '\n')
+					j++;
+				if (*j != '\0')
 				{
+					for (i = 0; i < 26; i++)
+					{
 					if (*j == small[i])
 					{
 						*j = caps[i];
 						break;
 					}
-				
+					}
 				}
 				break;
 			}
-		if (prev_delimiter && *j != '\0' && *j != ' ' && *j != '\t' && *j != '\n')
-	       	{
-			prev_delimiter = 0;
 		}
-		j++;:
+		j++;
 	}
-
-}
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	return (ptr);
 }
