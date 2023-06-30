ca5acff5af64540828dc30e0f7e0422dca194237
kiptoo haron <kiptooharon.hk@gmail.com>
2023-06-30 05:21:09 +0300

    function to append to start of a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _strncpy - append string to the start of a string
  * @dest: - destination string
@@ -22,37 +22,4 @@ char *_strncpy(char *dest, char *src, int n)
 		i++;
 	}
 	return (ptr);
-}
-int main(void)
-{
-    char s1[98];
-    char *ptr;
-    int i;
-
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
-    return (0);
 }
