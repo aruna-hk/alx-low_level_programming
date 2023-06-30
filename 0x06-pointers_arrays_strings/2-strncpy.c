26004bd0e7b750ab1b19da63cb0d6e57f98b5f9e
kiptoo haron <kiptooharon.hk@gmail.com>
2023-06-30 05:28:50 +0300

    function to append to start of a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	char *ptr = src;
+	char *ptr = dest;
 
+	dest+1;
 	while (i < n && *src != '\0')
 	{
 		*dest = *src;
-		dest++;
 		src++;
+		dest++;
 		i++;
 	}
-	dest = ptr;
 	return (dest);
 }
