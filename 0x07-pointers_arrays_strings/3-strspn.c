2025ac351ef426f51c311f07af1e35d966bad6b1
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-04 02:50:10 +0300

    function -- return no of bytes until which specific char inacceot str is found'

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+* _strspn - locates a character in string s and give byte locatin ,,
+* @s: --string to search for char in str accept
+* @accept: -- str -- source of search charcters
+* Return: byte location of char in accpt fro bgnin of str s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
 
+	while (*accept != '\0')
+	{
+		count++;
+		while (*s != '\0')
+		{
+			s++;
+		}
+		accept++;
+	}
+	return (count + 1);
+}
