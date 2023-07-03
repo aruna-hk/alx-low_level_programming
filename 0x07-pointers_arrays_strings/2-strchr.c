5ab6ebbf0e9037386d85dd59a0778b98b238b6fd
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-04 00:00:57 +0300

    function to locates a character in a string--return ptr to the end of the string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
 * _strchr - locates a character in a string--
 * @s: --string to search
@@ -7,13 +7,32 @@
 */
 char *_strchr(char *s, char c)
 {
+	int len = 0;
+
 	while (*s != '\0')
 	{
+		len++;
 		if (*s == c)
 		{
 			return (s);
 		}
 		s++;
+		if (*s == '\0')
+			s = s - (len + 1);
 	}
 	return (0);
 }
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
+}
+
