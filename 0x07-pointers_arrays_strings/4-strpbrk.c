d1ed610a574124bdd0b29c2722b003a503833c16
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-04 00:17:23 +0300

    function --looks for 1st occurence of any character of accept in string s --

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 /**
 * _strpbrk -looks for 1st occurence of any character of accept in string s
 * @s: str s--occurence look-up;
-* @accept: str-
+* @accept: str-to compat=re its caharacter with  s- string
 * Return: ptr to 1st occurence of char in str s;
 */
 char *_strpbrk(char *s, char *accept)
@@ -14,6 +14,7 @@ char *_strpbrk(char *s, char *accept)
 		len_accept++;
 		accept++;
 	}
+	accept = accept - (len_accept + 1);
 	while (*s != '\0')
 	{
 		while (*accept != '\0')
