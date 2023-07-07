294a5b7e596356ee05f9984036ad54356619983a
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-08 00:02:13 +0300

    main -- counts no of arg passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - count no of arguemets passed
+* @argc: name
+* @argv: arguements
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
