3fceabe3b146e0d9a5b636c335c46d2b3c70331d
kiptoo <kiptooharon.hk@gmail.com>
2023-07-09 01:59:31 +0300

    finds denominations of a given number

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,13 +1,49 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include "main.h"
 /**
-* main - prints the minimum number of coins to make change for an amount of money.
+* coins_denomination - find the denominations recursively
+* @deno: ptr to denomination lookup
+* @amt: number to check its denominations
+* @coins: denominations added
+* Return: 0
 */
-int coins_denomination(int *deno, int amt, int coins);
+int coins_denomination(int *deno, int amt, int *coins)
+{
+	if (amt == *deno)
+	{
+		return (*coins + 1);
+	}
+	else if (amt < *deno)
+	{
+		deno++;
+		return (coins_denomination(deno, amt, coins));
+	}
+	else
+	{
+		int rem = amt % *deno;
+
+		if (rem != 0)
+		{
+			*coins += amt / *deno;
+			deno++;
+		}
+		else
+			*coins += amt / *deno - 1;
+		return (coins_denomination(deno, (rem), coins));
+	}
+}
+/**
+* main - prints the minimum number of
+*  coins to make change for an amount of money.
+* @argc: numbe of arguements
+* @argv: lists of ptrs to args
+* Return: 0 sucess 1-failure
+*/
+
 int main(int argc, char *argv[])
 {
-	int deno[] = {25,10,5,2,1};
+	int deno[] = {25, 10, 5, 2, 1};
 	int *ptr = deno;
 	int amt;
 	int coins = 0;
@@ -16,7 +52,7 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (1);
-	}	
+	}
 	amt = atoi(argv[1]);
 	if (amt < 0)
 	{
@@ -24,31 +60,7 @@ int main(int argc, char *argv[])
 		putchar('\n');
 		return (1);
 	}
-	coins = coins_denomination(ptr,amt, coins);
-	printf("%d\n",coins);
+	coins = coins_denomination(ptr, amt, &coins);
+	printf("%d\n", coins);
 	return (0);
-} 
-int coins_denomination(int *deno, int amt, int coins)
-{
-	if (amt == *deno)
-	{
-		coins++;
-		return coins;
-	}
-	else if (amt < *deno)
-	{
-		deno++;
-		return coins_denomination(deno, amt, coins);
-	}
-	else
-	{
-		int rem = amt % *deno;
-		if (rem != 0)
-		{
-			coins += amt / *deno;
-			deno++;
-		}
-		return coins_denomination(deno, (rem), coins);
-	}   
 }
-
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,4 @@
 int _putchar(char *c);
+int coins_denomination(int *deno, int amt, int *coins);
+int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
