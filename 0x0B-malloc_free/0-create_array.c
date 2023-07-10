35a695107a7e08f71c8783658b49e4a5c3fa02a0
kiptoo <kiptooharon.hk@gmail.com>
2023-07-10 20:32:27 +0300

    creat string in heap initialize it with character --return ptr to string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (0);
-	init_ptr = malloc(sizeof(char)i * size);
+	init_ptr = malloc(sizeof(char) * size);
 	if (init_ptr == 0)
 		return (0);
 	for (i = 0; i < size; i++)
