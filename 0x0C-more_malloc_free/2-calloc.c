fecbd8972bd189ad9c31917fa9a4d3589bd86feb
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-14 02:02:54 +0300

    create array of elements with user defined size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	arr = malloc(size * nmemb);
-	if (arr == 0)
+	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
