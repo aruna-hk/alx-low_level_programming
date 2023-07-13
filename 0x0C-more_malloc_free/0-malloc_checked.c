85c0c1e4a8cc3b782935bc439163eb8db9cf364d
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-13 16:57:31 +0300

    allocated space in heap to passed unsigned int arg and return void ptr -- if unable exit with 98

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* malloc_checked - allocate space in the heap for the arg -- store
+* the arg in the space allocated & return pointer to the mem location
+* @b: unsigned int agruement
+* Return: void heap_space
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *heap_space;
+
+	heap_space = malloc(sizeof(int));
+	if (heap_space == NULL)
+		exit(98);
+	return (heap_space);
+}
