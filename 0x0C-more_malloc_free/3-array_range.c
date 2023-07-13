7788a8d2e627b2a0a549347796cf5214b5d03e08
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-14 00:28:27 +0300

    creates an array of integers-in the heap

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "maim.h"
+#include <stdlib.h>
+/**
+* array_range - takes int interval and insert them
+* into arr in heap & return ptr to the heap
+* @min: lower range
+* @max: higher range
+* Return: ptr to heap_space --array of elements
+*/
+int *array_range(int min, int max)
+{
+	int *arr;
+	int *ptr;
+
+	if (min > max)
+		return (0);
+	arr = malloc(sizeof(int) * (max - min + 1));
+	if (arr == 0)
+		return (0);
+	ptr = arr;
+	while (min <= max)
+	{
+		*arr = min;
+		arr++;
+		min++;
+	}
+	return (ptr);
+}
