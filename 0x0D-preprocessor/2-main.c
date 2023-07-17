55fee8a937a36999572d73fb238abda44af6d630
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-17 15:32:05 +0300

    dispalaying the name of prog file using predifided file macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+* _puts - display the str passed
+* @str: -take the string
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
