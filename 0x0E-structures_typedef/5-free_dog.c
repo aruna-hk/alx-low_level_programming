bf0fc89dbc637ef7390ca6db668d3fffc3a4d757
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-18 02:07:16 +0300

    free space occupied by dog struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+* free_dog - free dog struct in the memory
+* @d: takes dog struct
+**/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
