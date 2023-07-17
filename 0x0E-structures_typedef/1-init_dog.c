70e1fe1141690f0b76b362bb9b443b7c172d9824
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-17 20:30:59 +0300

    initializing elements of a structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "dog.h"
-#include <stdlib.h>
 /**
 * init_dog - initialize variables in structure -dog structure
 * @d: ptr to the structure
@@ -9,9 +9,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
