6808ab1efa09f4eb074fed1c3ad7b7d4455538db
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-17 20:35:39 +0300

    initializing elements of a structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "dog.h"
 /**
 * init_dog - initialize variables in structure -dog structure
@@ -9,10 +8,7 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
