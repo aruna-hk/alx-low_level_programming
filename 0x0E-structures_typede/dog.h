15ddcd8c218f2fc3440a3ad406936a8d0d71e825
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-17 13:57:18 +0300

    Defining c structures

---
+++ 0x0E-structures_typede/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+* struct dog - dog attributes
+* @name: store name of dog
+* @age: age of the dog
+* @owner: --
+* Description: dog structure --attributes of a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
