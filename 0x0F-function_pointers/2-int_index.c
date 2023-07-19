ebeb3f1fd5b8a4dedf1a927ff1febddf8015812c
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-19 14:13:45 +0300

    initializing a function with every element in array via function ptr

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+* int_index - iterate through array and pass every
+* element of arr a function
+* @array:array of elements to pass to a function
+* @size: size of array
+* @cmp: function pointer
+* Return: -1 failure, index
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int c;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		c = (*cmp)(array[i]);
+		if (c != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
