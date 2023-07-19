82bf0ae7092b6660fe8fa425272e9614b19f9ba3
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-19 20:33:08 +0300

    dereferencing compare function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,9 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 	int c;
 
-	if (array == NULL || cmp == NULL)
-		return (0)
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	while (i < size)
 	{
