6d111a7491604b9d72804cb7f295ef6af4855306
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-20 00:52:14 +0300

    accesing and printing content of memory

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main -- print content of memory --where function resides
+* @argc: number of arguements
+* @argv: array of pointers to arguements
+* Return: 0--SUCCESS/OTHER EXIT CODES 1 - less arg, 2 < 0 bytes arg
+*/
+int main(int argc, char *argv[])
+{
+	unsigned int i = 0;
+	size_t size;
+
+	char *memoryadd = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	size = atoi(argv[1]);
+	if (size < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (i < size)
+	{
+		printf("%x", memoryadd[i]);
+		if (i != (size - 1))
+			printf(" ");
+		if (i == (size - 1))
+			printf("\n");
+		i++;
+	}
+	return (0);
+}
+
