a521f2d0cd6c7782e53bd5811df0df234dcbfd66
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-20 12:56:20 +0300

    performing sum of int arguements to variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+* sum_them_all - sum int arguements to a variadic function
+* @n: number of passes
+* Return:sum/0
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i = 0;
+	int sum = 0;
+
+	va_start(numbers, n);
+	while (i < n)
+	{
+		sumi += va_arg(numbers, int);
+		i++;
+	}
+	va_end(numbers);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,7 @@
+----learning-----LESSON VARIADIC FUNCTIONS--------------
+---------VARIADIC FUNCTIONS-------------------
+Author <kiptooharon.hk@gmail.com>
+variadic functions -------take variable number of arguements
+use case-----printf
+-----------scanf,fprintf  etc
+
