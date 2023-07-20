1009ccf44c4d424d39c685087cb71975e484639f
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-21 00:28:26 +0300

    function to print string arguements passed to variadic function'--- updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -40,13 +40,3 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_end(strings);
 	putchar('\n');
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_strings(", ", 2, "Jay", "Django");
-    return (0);
-}
