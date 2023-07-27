9a995979157d7e18e566ce87edfcce7fcb16a268
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-28 00:56:18 +0300

    constructor in c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+* constructor - executed before main function;
+*/
+void constructor(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
