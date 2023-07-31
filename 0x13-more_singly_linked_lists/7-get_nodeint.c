f86407d660ef849a65104ff5a1d9b5f64051b6b5
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-31 23:31:17 +0300

    find and return value of provided index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	size_t count = 0;
 
+	if (head == NULL)
+		return (NULL);
 	while (temp->next != NULL)
 	{
 		if (count == index)
