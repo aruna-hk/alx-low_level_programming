d8ac77d27f8b991b01a0204de3f60f28dc99ad90
kiptoo haron <kiptooharon.hk@gmail.com>
2023-07-31 21:47:39 +0300

    funct to free list via double ptr

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,17 +5,14 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
-	if (*head == NULL)
-	{
-		free(head);
-	}
 	while (*head != NULL)
 	{
-		temp = *head;
+		tmp = *head;
 		*head = (*head)->next;
-		free(temp);
+		free(tmp);
+
 	}
 
 }
