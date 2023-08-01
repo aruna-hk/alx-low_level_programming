55f62f8ba3330462e2732c2dce493ca2faa8dad0
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-02 01:15:27 +0300

    updated function----to remove node at given index -- update--

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = NULL;
 	listint_t *temp2 = NULL;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
 	temp = *head;
 	while (temp != NULL)
