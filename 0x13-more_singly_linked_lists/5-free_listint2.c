a26420b4b18efbf1c9d8df1de49a960349466cdb
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-01 15:10:04 +0300

    fun to free linked list--update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 /**
 * free_listint2 - free list
 * @head: double ptr to list
@@ -8,7 +7,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
 	while (*head != NULL)
 	{
