2597a14a49bf0ea021c56a6329cbc61ad706995a
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-01 19:29:41 +0300

    update of function to insert provided index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,28 +1,5 @@
 #include "lists.h"
 /**
-* add_nodeint - add node at the beggining of the list
-* @head: ptr to the head of the list
-* @n: nterger number to be inserted in the new node
-* Return: null is malloc fails,, ptr to new node
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (*head == NULL)
-	{
-		*head = new;
-		new->next = NULL;
-		return (*head);
-	}
-	new->next = *head;
-	*head = new;
-	return (*head);
-}
-/**
 * insert_nodeint_at_index - insert node at give index
 * @head: ptr to head double
 * @idx: index to insert
@@ -31,40 +8,36 @@ listint_t *add_nodeint(listint_t **head, const int n)
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	size_t pos = 0;
-	size_t nodes = 0;
 	listint_t *temp = *head;
-	listint_t *newnode = malloc(sizeof(listint_t));
+	unsigned int count = 0;
+	listint_t *new = malloc(sizeof(listint_t));
 
-	if (newnode == NULL)
+	if (new == NULL)
 		return (NULL);
-	newnode->n = n;
-	newnode->next = NULL;
-
-	if (*head == NULL)
-		return (NULL);
-	while (temp != NULL)
+	if (head == NULL || *head == NULL)
 	{
-		nodes++;
-		temp = temp->next;
+		free(new);
+		return (NULL);
 	}
-	if (nodes < (idx + 1))
-		return (NULL);
-	temp = *head;
+	new->n = n;
+	new->next = NULL;
 	if (idx == 0)
-		return (add_nodeint(head, n));
-	if (nodes == (idx))
-		return (add_nodeint_end(head, n));
+	{
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
 	while (temp != NULL)
 	{
-		if (pos == (idx - 1))
+		if (count == idx - 1)
 		{
-			newnode->next = temp->next;
-			temp->next = newnode;
+			new->next = temp->next;
+			temp->next = new;
 			return (temp);
 		}
-		pos++;
+		count++;
 		temp = temp->next;
 	}
+	free(new);
 	return (NULL);
 }
