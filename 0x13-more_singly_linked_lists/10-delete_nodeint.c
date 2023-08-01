83b69c6f44f1645b33dc9d3c04ea23d21e42ea43
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-01 22:35:42 +0300

    update of func to delete from give index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *temp2;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	while (temp->next != NULL)
 	{
