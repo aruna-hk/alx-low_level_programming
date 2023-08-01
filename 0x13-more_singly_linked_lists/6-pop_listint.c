2d24261049384c4a332ae17e652066b65f4f81c7
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-01 15:27:47 +0300

    -update to pop func-- function to remove first element frm list'

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,13 +7,15 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n;
 
 	if (head == NULL || *head == NULL)
 	{
 		return (0);
 	}
+	n = (*head)->n;
 	temp = *head;
 	*head = temp->next;
 	free(temp);
-	return ((*head)->n);
+	return (n);
 }
