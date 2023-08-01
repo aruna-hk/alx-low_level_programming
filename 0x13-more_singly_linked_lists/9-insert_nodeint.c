60839fc5ee3c650f31f07c5866e004f4a3a5d245
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-02 02:32:59 +0300

    update of insertion function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,18 +12,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 	listint_t *new;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if (idx == 0)
+	if (*head == NULL)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		if (idx == 0)
+		{
+			new->next = *head;
+			*head = new;
+			return (new);
+		}
+		else
+			return (NULL);
 	}
 	temp = *head;
 	while (temp != NULL)
