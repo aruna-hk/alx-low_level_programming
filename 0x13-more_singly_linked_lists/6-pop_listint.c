ad0ae906359f069507113d2549bb7c81f5958e63
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-01 23:08:47 +0300

    function to remove 1st node of a list--update

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,13 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int n;
 
-	if (!head || !*head)
+	if (*head == NULL)
 		return (0);
 
 	n = (*head)->n;
 	tmp = (*head)->next;
 	free(*head);
 	*head = tmp;
-
 	return (n);
 }
 
