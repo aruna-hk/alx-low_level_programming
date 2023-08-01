64b9b6f3861543bb86b4b7032c403be226ed3740
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-02 02:00:58 +0300

    inserting node at providex index--update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,25 +8,24 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp = *head;
+	listint_t *temp = NULL;
 	unsigned int count = 0;
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if (head == NULL || *head == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
+	temp = *head;
 	while (temp != NULL)
 	{
 		if (count == idx - 1)
