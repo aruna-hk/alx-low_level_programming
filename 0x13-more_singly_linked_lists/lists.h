b1b7963bf3aded40d86539f3468e1db401c7f004
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-02 15:17:46 +0300

    update of header file lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stddef.h>
 #include <unistd.h>
 /**
@@ -19,11 +20,15 @@ typedef struct listint_s
 int _putchar(char c);
 int pop_listint(listint_t **head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+listint_t *find_listint_loop(listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 void free_listint2(listint_t **head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int sum_listint(listint_t *head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 void print_number(int n);
 size_t listint_len(const listint_t *h);
 void free_listint(listint_t *head);
