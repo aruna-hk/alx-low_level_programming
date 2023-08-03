54ab320e0cb97128c1bc83d9d4fd86b6fef772db
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-03 19:47:48 +0300

    function to find number of bits of a number to flip to get to another number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* flip_bits - determine number of bits to flip to get to give number
+* @n: int arg
+* @m: int arg
+* Return: number of bits to flip
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int default_mask = 1;
+	unsigned int i = 0;
+	unsigned int count = 0;
+	unsigned int difference = n ^ m;
+
+	while (i < (sizeof(unsigned int) * 8))
+	{
+		if ((difference & (default_mask << i)) != 0)
+		{
+			count++;
+		}
+		difference = difference & (~(default_mask << i));
+		if (difference == 0)
+			return (count);
+		i++;
+	}
+	return (0);
+}
