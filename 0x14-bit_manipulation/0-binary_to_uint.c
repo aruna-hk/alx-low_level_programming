513d05c69f12988798724aa58b24f9faa2e2bfb2
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-04 00:14:03 +0300

    conversion of binary to interger

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,5 @@
 #include "main.h"
-/**
-* _strlen - check string legthn
-* @s: pointer to array of chaacters
-* Return: len-length of the string
-*/
-unsigned int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len = len + 1;
-		s++;
-	}
-	return (len);
-}
-
+#include <string.h>
 /**
 * binary_to_uint - convert binary to unsigned int
 * @b: string of bits
@@ -23,41 +7,41 @@ unsigned int _strlen(const char *s)
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	unsigned int default_n = 1;
 	unsigned int i = 0;
-	unsigned int characters;
-	char *str;
+	unsigned int default_num = 1;
+	unsigned int num = 0;
+	int characters;
+	char *str, *ptr;
 
 	if (b == NULL)
 		return (0);
-
-	str = malloc(sizeof(_strlen(b)));
+	str = malloc(strlen(b) + 1);
 	if (str == NULL)
 		return (0);
-
-	characters = _strlen(b) - 1;
-	while (characters > 0)
+	characters = strlen(b) - 1;
+	while (characters >= 0)
 	{
 		str[i] = b[characters];
-		i++;
 		characters--;
+		i++;
 	}
-	str[i] = b[0];
-	i++;
 	str[i] = '\0';
 	i = 0;
-	while (i < _strlen(b))
+	ptr = str;
+	while (*str != '\0' && (i < (sizeof(unsigned int) * 8)))
 	{
-		if (str[i] != '0' && str[i] != '1')
+		if (*str != '0' && *str != '1')
 		{
+			free(ptr);
 			return (0);
 		}
-		if (str[i] == '1')
+		if (*str == '1')
 		{
-			num += default_n << i;
+			num = num | (default_num << i);
 		}
 		i++;
+		str++;
 	}
+	free(ptr);
 	return (num);
 }
