f1a8b09263ffe34d2b5bd49cc2327df7868e22fb
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-07 16:17:41 +0300

    function to read text files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* read_textfile - read ascii file
+* @filename: - file to read
+* @letters:- number of characters to read
+* Return: 1-success -1-falure
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t n;
+	ssize_t read_to_buffer;
+
+	ssize_t file_descript = open(filename, O_RDONLY);
+
+	if (file_descript == -1)
+		return (0);
+	buffer = malloc(letters);
+	read_to_buffer = read(file_descript, buffer, letters);
+	if (read_to_buffer == -1)
+		return (0);
+	n = write(1, buffer, read_to_buffer);
+	if (n == -1)
+		return (-1);
+	return (n);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+FILE HANDLING IN C
+author <kiptooharon.hk@gmail.com>
+files --
+--0-read_textfile.c;
