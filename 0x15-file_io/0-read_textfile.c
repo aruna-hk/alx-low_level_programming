6365487d1f29bc2d01d72ec640a352c54cf9b96f
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-07 18:31:15 +0300

    function to read content of the file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t n;
 	ssize_t read_to_buffer;
+	ssize_t file_descript;
 
-	ssize_t file_descript = open(filename, O_RDONLY);
+	if (filename == NULL)
+		return (0);
+	file_descript = open(filename, O_RDONLY);
 
 	if (file_descript == -1)
 		return (0);
 	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
 	read_to_buffer = read(file_descript, buffer, letters);
 	if (read_to_buffer == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 	n = write(1, buffer, read_to_buffer);
 	if (n == -1 || n != read_to_buffer)
-		return (-1);
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
 	return (n);
 }
