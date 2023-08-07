669a86c828b467f377a7820d6c8adaab3312c4e5
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-07 19:14:57 +0300

    update of function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (filename != NULL && text_content == NULL)
+	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT, 0600);
 		if (fd == -1)
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
