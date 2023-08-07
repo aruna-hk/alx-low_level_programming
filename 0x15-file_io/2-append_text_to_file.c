8377c041ff051f9528ccc68c5fd0935fd970dbae
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-07 17:11:50 +0300

    correction of file to append text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,16 +1,4 @@
 #include "main.h"
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /**
 * _strlen - check string legthn
 * @s: pointer to array of chaacters
@@ -40,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+	if (filename != NULL && text_content == NULL)
 		return (1);
 
 	fd = open(filename, O_RDWR | O_APPEND);
