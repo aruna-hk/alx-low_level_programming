494c1b53f9ea9462577ff6c5596c9c707a0052c2
kiptoo haron <kiptooharon.hk@gmail.com>
2023-08-07 19:06:31 +0300

    function to create a file /trancate file content if file exitst-update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -45,7 +45,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	n = write(fd, text_content, _strlen(text_content));
-	if (n == _strlen(text_content))
+	if (n != _strlen(text_content))
 	{
 		close(fd);
 		return (-1);
