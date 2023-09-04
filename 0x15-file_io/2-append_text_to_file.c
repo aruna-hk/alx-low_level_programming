10581ba6aa97812dbc9d5b3fbf12c9dd7f882a6e
kiptoo haron <kiptooharon.hk@gmail.com>
2023-09-04 20:59:35 +0300

    function to append text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	fd2 = write(fd, text_content, strlen(text_content) + 1);
+	fd2 = write(fd, text_content, strlen(text_content));
 	if (fd2 == -1)
 		return (-1);
 	close(fd);
