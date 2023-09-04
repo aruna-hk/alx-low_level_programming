f94343c1134fefa035bfba172f07e36a775f6c2f
kiptoo haron <kiptooharon.hk@gmail.com>
2023-09-04 22:18:32 +0300

    updated memory management in in read function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,19 +12,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL || letters == 0)
 		return (0);
-	buffer = malloc(letters);
-	if (buffer == NULL)
-		return (0);
 	filedes = open(filename, O_RDONLY);
 	if (filedes == -1)
 		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
 	nread = read(filedes, buffer, letters);
 	if (nread == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 	close(filedes);
 	nwrite = write(1, buffer, nread);
 	if (nwrite != nread)
+	{
+		free(buffer);
 		return (0);
+	}
 	free(buffer);
 	return (nwrite);
 }
