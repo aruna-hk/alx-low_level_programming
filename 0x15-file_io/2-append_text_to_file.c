21dc33177a254f0f6935a86ab66dfbab7caa67aa
kiptoo haron <kiptooharon.hk@gmail.com>
2023-09-04 20:48:59 +0300

    update of function to appedn text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,26 @@
 #include "main.h"
 /**
-* _strlen - check string legthn
-* @s: pointer to array of chaacters
-* Return: len-length of the string
-*/
-int _strlen(char *s) 
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len = len + 1;
-		s++;
-	}
-	return (len);
-}
-
-/**
 * append_text_to_file - function that append txt to a file
 * @filename: file to append txt
 * @text_content: what to append to the end of file
+* Return: 1-sucess -1 failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, fd2, len;
+	int fd, fd2, len = 0;
+	char *c = text_content;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_APPEND);
 	if (fd == -1 || text_content == NULL)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	len = _strlen(text_content);
-	fd2 = write(fd, text_content, len);
-	if (fd2 == -1 )
+	while (*c != '\0')
+		len++;
+	fd2 = write(fd, text_content, len + 1);
+	if (fd2 == -1)
 		return (-1);
 	return (1);
 }
