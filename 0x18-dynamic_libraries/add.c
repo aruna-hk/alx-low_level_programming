c3299f01b586dcd116f3c1a7609a1a5c898b265f
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-16 15:33:45 -0400

    created dynamic library for arithmethic operation calleble in python

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,51 @@
+/**
+* sum - perform addition
+* @a: first operand
+* @b: second operand
+* Return: a+b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* mod - perform addition
+* @a: first operand
+* @b: second operand
+* Return: a+b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+* div  - perform addition
+* @a: first operand
+* @b: second operand
+* Return: a+b
+*/
+int div(int a, int b)
+{
+	int dev = a/b;
+	return (dev);
+}
+/**
+* sub  - perform addition
+* @a: first operand
+* @b: second operand
+* Return: a+b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* mul  - perform addition
+* @a: first operand
+* @b: second operand
+* Return: a+b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
