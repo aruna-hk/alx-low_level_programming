8d4cec95eb73a98d87c8936d3a50ac4c72d71b46
kiptoo <kiptoharon.hk@gmail.com>
2023-11-03 11:43:26 -0400

    update of function that adds entry to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	unsigned long int idx;
 
-	if (key == NULL || value == NULL)
+	if (key == NULL || value == NULL || ht == NU;;)
 		return (0);
 	idx = key_index((const unsigned char *) key, ht->size);
 	if (idx > ((ht->size) - 1))
