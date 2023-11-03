5372f39bbebccdc8c6cba8ea1d5b7f36a88373da
kiptoo <kiptoharon.hk@gmail.com>
2023-11-03 12:41:15 -0400

    update of function to add recors to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,43 @@
 #include "hash_tables.h"
 /**
+* search_add - search if key already present
+* @ht: hash table ptr
+* @new: new node to add to hash table
+* @idx: index of hash tble to search
+*/
+void search_add(hash_table_t *ht, hash_node_t *new, unsigned long int idx)
+{
+	hash_node_t *bgn = ht->array[idx];
+
+	while (ht->array[idx]->next != NULL)
+	{
+		if (strcmp(ht->array[idx]->key, new->key) == 0)
+		{
+			free(ht->array[idx]->value);
+			ht->array[idx]->value = strdup(new->value);
+			free(new->key);
+			free(new->value);
+			free(new);
+			ht->array[idx] = bgn;
+			return;
+		}
+		ht->array[idx] = ht->array[idx]->next;
+	}
+	if (strcmp(ht->array[idx]->key, new->key) == 0)
+	{
+		free(ht->array[idx]->value);
+		ht->array[idx]->value = strdup(new->value);
+		free(new->key);
+		free(new->value);
+		free(new);
+		ht->array[idx] = bgn;
+		return;
+	}
+	ht->array[idx] = bgn;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+}
+/**
 * hash_table_set - add value to the hash  table
 * @ht: pointer to hash table
 * @key: key to determine the index at which the value is to e stored
@@ -17,19 +55,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (idx > ((ht->size) - 1))
 		return (0);
 	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
-
 	if (ht->array[idx] == NULL)
 		ht->array[idx] = new;
 	else
-	{
-		new->next = ht->array[idx];
-		ht->array[idx] = new;
-	}
+		search_add(ht, new, idx);
 	return (1);
 }
