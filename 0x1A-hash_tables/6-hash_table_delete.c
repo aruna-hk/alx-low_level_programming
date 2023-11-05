6fdf7ca6dce87ac1f16f6975766c57f115fead6c
kiptoo <kiptooharon.hk@gmail.com>
2023-11-05 08:35:51 -0500

    correction of function to free hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,36 @@
 #include "hash_tables.h"
 /**
+* free_list - frees linked list
+* @head: list ptr
+*/
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->key);
+		free(head->value);
+		free(head);
+		head = temp;
+	}
+}
+/**
 * hash_table_delete - free hash table
 * @ht: ptr to hash table
 */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp;
-
 	unsigned long int i = 0;
 
 	if (ht == NULL)
 		return;
-	if (ht->size == 0)
-	{
-		free(ht);
-		return;
-	}
 	while (i < (ht->size))
 	{
-		while (ht->array[i] != NULL)
-		{
-			tmp = ht->array[i];
-			ht->array[i] = ht->array[i]->next;
-			free(tmp);
-		}
-		free(ht->array[i]);
+		free_list(ht->array[i]);
 		i++;
 	}
 	free(ht->array);
